1eda4b81009649cb9855a3084b2c643321bd54a1
alexandralanorias <97576137+alexandralanorias@users.noreply.github.com>
2023-05-04 15:42:46 +0800

    Update About.jsx

--- pages/About.jsx
+++ pages/About.jsx
@@ -7,12 +7,12 @@ export default function About() {
         <div className="about-page-container">
             <img src={bgImg} className="about-hero-image" />
             <div className="about-page-content">
-                <h1>Don’t squeeze in a sedan when you could relax in a album.</h1>
-                <p>Our mission is to enliven your road trip with the perfect travel album rental. Our albums are recertified before each trip to ensure your travel plans can go off without a hitch. (Hitch costs extra 😉)</p>
-                <p>Our team is full of albumlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
+                <h1>Can't find the perfect place to grab a classical track?</h1>
+                <p>Worry no more! Our mission is to add a dramatic flair to your listening experience with the perfect classical recording. From Bach to Beethoven, Mozart to Chopin, and even Shostakovich to Bartók, our albums are tried and tested must-haves for classical music enthusiasts on the search for a new rental or even purchase. (Purchases costs extra 😉)</p>
+                <p>Our team is full of classical music aficionados who know firsthand the magic of listening to, take for example, a vinyl recording of Tchaikovsky's Fifth Symphonyin E minor, performed by the New York Philharmonic under the skillful baton of Leonard Bernstein.</p>
             </div>
             <div className="about-page-cta">
-                <h2>Your destination is waiting.<br />Your album is ready.</h2>
+                <h2>Your listening experience is waiting.<br />Your album is ready.</h2>
                 <Link className="link-button" to="/albums">Explore our albums</Link>
             </div>
         </div>
