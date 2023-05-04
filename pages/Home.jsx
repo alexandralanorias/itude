7d195ed9aa8b6a9bd72af88fda7c85533753914d
alexandralanorias <97576137+alexandralanorias@users.noreply.github.com>
2023-05-04 15:12:48 +0800

    Modifying home page

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom"
 export default function Home() {
     return (
         <div className="home-container">
-            <h1>You got the travel plans, we got the travel albums.</h1>
-            <p>Add adventure to your life by joining the #albumlife movement. Rent the perfect album to make your perfect road trip.</p>
+            <h1>You got the cash, we got the stash.</h1>
+            <p>Listening, con brio. Rent or buy the perfect album and add it to your classical music repertoire.</p>
             <Link to="albums">Find your album</Link>
         </div>
     )
